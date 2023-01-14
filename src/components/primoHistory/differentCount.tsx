fb56af66a2e36f3e1c84c2262367c22317184715
Spudisis <alekasisbb@gmail.com>
2023-01-14 21:16:15 +0300

    fix view DifferentCount

--- src/components/primoHistory/differentCount.tsx
+++ src/components/primoHistory/differentCount.tsx
@@ -9,7 +9,7 @@ export const DifferentCount = ({ count }: different) => {
       {count > 0 ? (
         <div className={`${s.change} ${s.up}`}>+{count}</div>
       ) : count < 0 ? (
-        <div className={`${s.change} ${s.down}`}>-{count}</div>
+        <div className={`${s.change} ${s.down}`}>{count}</div>
       ) : (
         <div className={`${s.change} ${s.normal}`}>{count}</div>
       )}
