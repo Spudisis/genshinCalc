0fdda9b27aaf4df586ee0eed3cd729ba95360587
Spudisis <alekasisbb@gmail.com>
2023-01-19 22:37:34 +0300

    pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { App } from "./App";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persister } from "./store/store";
-import { StrictMode } from "react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
