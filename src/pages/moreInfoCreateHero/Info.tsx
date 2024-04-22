ba9e84ad0ff5d81e0cf62cd22aefd0efdc045f89
spudisis <alekasisbb@gmail.com>
2024-04-22 20:02:33 +0300

    fix

--- src/pages/moreInfoCreateHero/Info.tsx
+++ src/pages/moreInfoCreateHero/Info.tsx
@@ -1,6 +1,6 @@
 
 import s from "./Info.module.scss";
-import addHero from "../../assets/info/addHero.png";
+import addHero from "@/assets/info/addHero.PNG";
 export const Info = () => {
   return (
     <div className={s.root}>
